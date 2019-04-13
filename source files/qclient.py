5bd6413ca9c187ccc764a247dcdb3ca263f30bf6
Stephen <stephenpaladines@gmail.com>
2019-04-13 18:33:52 -0400

    major overhaul
    -Modulated Code
    
    -Fixed missing data
    
    -Cleaned/remove
    
    -Added Qmeister.py

--- source files/qclient.py
+++ source files/qclient.py
@@ -7,23 +7,31 @@ import socket # Library used for socket connection
 import sys    # Library used for error handling
 
 # Catches an error with binding the socket to localhost and port #
+
+socketObj = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Socket object
 try:
-    socketObj = socket.socket()         # Socket object
-    socketObj.connect(('',4567))  # Connection to server
+    host = sys.argv[1]
+    port = sys.argv[2]
+except:
+    print("Missing arguments. Please try again.......")
+    exit()
+
+try:
+    socketObj.connect((host,int(port)))        # Connection to server
 except socket.error as error_message:
     print('Socket unsucessful\n' + 'Error Code:' + str(error_message[0]) + 'Error Message:' + str(error_message[1]))
     sys.exit();
 
 print('Socket was successful')    # Success servInput prints if previous passed
-servInput = ''
-socketObj.recv(1024)
-socketObj.sendall('Client connected to socket')
+
 while True:
     servInput = raw_input('> ')
+
     if(len(servInput) > 0):
-        socketObj.sendall(servInput[0])       # Sends the first char in servInput
+        socketObj.send(servInput[0])       # Sends the first char in servInput
     else:
         continue
+
     if servInput == 'q':
         print('Closing client')
         socketObj.close()               #  Closes the socket from client-side
@@ -48,21 +56,21 @@ while True:
     elif servInput[0] == 'g':
         if len(servInput) < 2:            # Check if the argument is present
             print('Missing question number')
-            socketObj.sendall('-1')
+            socketObj.send('-1')
             continue
         else:
             servInput = servInput.split()
             j = 0
             while j < 500:
                 j+=1
-            socketObj.sendall(servInput[1])
+            socketObj.send(servInput[1])
             data = socketObj.recv(1024) # Receive response servInput
             print(data)
     elif servInput == 'r':
         data = socketObj.recv(1024) # Receive random question row
         print(data)
         userAnswer = raw_input()        # User answer to verify
-        socketObj.sendall(userAnswer)
+        socketObj.send(userAnswer)
         data = socketObj.recv(1024) # Receive response servInput (fail or success)
         print(data)
     elif servInput[0] == 'c':
@@ -74,7 +82,7 @@ while True:
             j = 0
             while j < 500:
                 j+=1
-            socketObj.sendall(servInput[1] + ' ' + servInput[2])
+            socketObj.send(servInput[1] + ' ' + servInput[2])
             print(servInput[1] + ' ' + servInput[2])
             data = socketObj.recv(1024) # Receive response servInput
             print(data)
@@ -82,9 +90,9 @@ while True:
         print('If you need help, choose one of the following charater:\n k,q,h,c,r,k,d,g,p')
     elif servInput == 'p':
         userInput = raw_input()         # Enter question tag
-        socketObj.sendall(userInput)
+        socketObj.send(userInput)
         userInput = raw_input()         # Enter question name
-        socketObj.sendall(userInput)
+        socketObj.send(userInput)
         print('.')                      # Print first dot
         counter = 0
         question = ""
@@ -96,9 +104,9 @@ while True:
                 print('.')
                 question += (userAnswer + '\n.\n')
                 counter += 1                # Verifies atleast two answers
-        socketObj.sendall(question)     # Sends answers
+        socketObj.send(question)     # Sends answers
         userCorrectAnswer = raw_input() # Enters valid answer
-        socketObj.sendall(userCorrectAnswer)
+        socketObj.send(userCorrectAnswer)
         data = socketObj.recv(1024) # Receive response servInput
         print(data)
     else:
--- source files/qserver.py
+++ source files/qserver.py
@@ -1,176 +1,213 @@
 #!/usr/bin/env python
-
 # Part of the socket program was adapted from https://www.geeksforgeeks.org/socket-programming-python/.
 # I do not claim to own this code. The code was used as a template for creating the socket between server and client
 import time              # Library used for delaying
 import socket            # Library used for socket connection
 import pip               # Library used to install the mysql package into the host machine
 import sys               # Library used for error handling
+import traceback
+from threading import Thread
 import mysql.connector   # Library used for mysql access
-from mysql.connector import MySQLConnection, Error
+from mysql.connector import MySQLConnection, Error, errorcode
 
 # The packet install code was based off user, Rikard Anglerud, on the following website
 # https://stackoverflow.com/questions/12332975/installing-python-module-within-code
 # The code allows for the mysql-connector-python package to be automatically installed in the users machine
 # Code IS NOT MINE and I DO NOT CLAIM IT AS MY OWN. CODE is used to install a moddule needed for MYSQL connection
 # Without the need for PIP
+# Creates a connection to a mysql server (Free DB provided by UF)
 
-def install(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])
-        if __name__ == '__main__':
-            install('mysql-connector-python')
+# def install(package):
+    # if hasattr(pip, 'main'):
+    #     pip.main(['install', package])
+    # else:
+    #     pip._internal.main(['install', package])
+    # # if __name__ == '__main__':
+    #     install('mysql-connector-python')
+# def client_thread(connection, ip, port, max_buffer_size = 5120):
+#     is_active = True
 
-# Catches an error with binding the socket to localhost and port #
-try:
+#     while is_active:
+#         client_input = receive_input(connection, max_buffer_size)
+
+#         if "--QUIT--" in client_input:
+#             print("Client is requesting to quit")
+#             connection.close()
+#             print("Connection " + ip + ":" + port + " closed")
+#             is_active = False
+#         else:
+#             print("Processed result: {}".format(client_input))
+#             connection.send("-".encode("utf8"))
+# def receive_input(connection, max_buffer_size):
+#     client_input = connection.recv(max_buffer_size)
+#     client_input_size = sys.getsizeof(client_input)
+
+#     if client_input_size > max_buffer_size:
+#         print("The input size is greater than expected {}".format(client_input_size))
+
+#     decoded_input = client_input.decode("utf8").rstrip()  # decode and strip end of line
+#     return decoded_input
+# def process_input(input_str):
+#     print("Processing the input received from client")
+
+#     return "Hello " + str(input_str).upper()
+# def contestant_input(connection):
+# connection.send("Hello World")
+
+def create_socket():
     # Socket object with AF_INET = IPV4  & SOCK_STREAM = TCP connect
     socketObj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketObj.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Binds the address, blank for localhost, and port shared between client & server
-    socketObj.bind(('',4567))
-except socket.error as error_message:
-    print('Socket unsucessful\n' + 'Error Code:' + str(error_message[0]) + '\nError Message:' + str(error_message[1]))
-    sys.exit();
-
-print('Socket was successful')    # Success message prints if previous passed
-socketObj.listen(1)               # Maximum number of connections that can be queued
-
-# Creates a connection to a mysql server (Free DB provided by UF)
-try:
-    dbObj = mysql.connector.connect(user='paladine', password='quizApp123', host='mysql.cise.ufl.edu', database='quizApp')
-    dbCursor =  dbObj.cursor()
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-print('Database connection is successful!')
-
-while True:
-    userInput = ""
-    conn,address = socketObj.accept()
-    conn.sendall('Connection established')
-    conn.recv(1024)
+    socketObj.bind(('',0))
+    global port
+    port = socketObj.getsockname()[1]
+    print(port)
+    print('Socket was successful')    # Success message prints if previous passed
+    socketObj.listen(5)               # Maximum number of connections that can be queued
+    return socketObj
+def db_setup():    
+    # install('mysql-connector-python')
+    try:
+        dbObj = mysql.connector.connect(user = 'paladine', password ='quizApp123', host ='mysql.cise.ufl.edu', database ='quizApp')
+    except mysql.connector.Error as err:
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+        sys.exit()
+      elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+        sys.exit()
+      else:
+        print(err) 
+    print('Database connection is successful!')
+    return dbObj
+def client_input(socketObj,dbObj):
     while True:
-        userInput = conn.recv(1024).strip()
-        print('received:' +  userInput)
-        if userInput == 'k':
-            # done
-            print('Shuting Server', address)
-            conn.close()
-            dbObj.close()
-            socketObj.shutdown(socket.SHUT_RDWR)
-            socketObj.close()
-            break
-        elif userInput == 'q':
-            # done
-            print('Client Closing')
-            conn.close()
-            break
-        elif userInput == 'p':
-            # done
-            print('Adding new question to db...')
-            userTag = conn.recv(1024).strip()
-            print(userTag + '\n')
-            userName = conn.recv(1024).strip()
-            print(userName + '\n')
-            userQue = conn.recv(1024).strip()
-            print(userQue + '\n')
-            userAnswer = conn.recv(1024).strip()
-            print(userAnswer + '\n')
-            print(userTag + userName + userQue + userAnswer)
-            try:
-                dbCursor.execute("INSERT INTO question (tag,name,questions,answer) VALUES (%s,%s,%s,%s)",(userTag,userName,userQue,userAnswer))
-                dbObj.commit()
-                dbCursor.execute("SELECT id FROM question ORDER BY id DESC LIMIT 1")
-                set = dbCursor.fetchall()
-                for row in set:
-                    conn.sendall("%d" % (row[0]))
-            except Error as error:
-                print(error)
-            print(dbCursor.rowcount, "record inserted.")
-        elif userInput == 'd':
-            # done
-            userQuestionNum = conn.recv(1024)
-            print(userQuestionNum)
-            if userQuestionNum == '-1':
-                continue
-            try:
-                dbCursor.execute("SELECT * FROM question where id = '%d'" %(int(userQuestionNum)))
-                set = dbCursor.fetchall()
-                if(len(set) < 1):
-                    conn.sendall('Question not found')
+        conn,address = socketObj.accept()
+        dbCursor =  dbObj.cursor()
+        while True:
+            userInput = conn.recv(1024).decode().strip()
+            if len(userInput) > 0:
+                if userInput == 'k':
+                    print('Shuting Server', address)
+                    conn.close()
+                    dbObj.close()
+                    socketObj.shutdown(socket.SHUT_RDWR)
+                    socketObj.close()
+                    sys.exit()
+                elif userInput == 'q':
+                    print('Client Closing')
+                    conn.close()
+                    break
+                elif userInput == 'b':
+                    contestantSocket = firstmakeConnection()
+                    start_new_thread(testFunction,(contestantSocket,))
+                elif userInput == 'p':
+                    # done
+                    print('Adding new question to db...')
+                    userTag = conn.recv(1024).strip()
+                    print(userTag + '\n')
+                    userName = conn.recv(1024).strip()
+                    print(userName + '\n')
+                    userQue = conn.recv(1024).strip()
+                    print(userQue + '\n')
+                    userAnswer = conn.recv(1024).strip()
+                    print(userAnswer + '\n')
+                    print(userTag + userName + userQue + userAnswer)
+                    try:
+                        dbCursor.execute("INSERT INTO question (tag,name,questions,answer) VALUES (%s,%s,%s,%s)",(userTag,userName,userQue,userAnswer))
+                        dbObj.commit()
+                        dbCursor.execute("SELECT id FROM question ORDER BY id DESC LIMIT 1")
+                        set = dbCursor.fetchall()
+                        for row in set:
+                            conn.send("%d" % (row[0]))
+                    except Error as error:
+                        print(error)
+                    print(dbCursor.rowcount, "record inserted.")
+                elif userInput == 'd':
+                    # done
+                    userQuestionNum = conn.recv(1024)
+                    print(userQuestionNum)
+                    if userQuestionNum == '-1':
+                        continue
+                    try:
+                        dbCursor.execute("SELECT * FROM question where id = '%d'" %(int(userQuestionNum)))
+                        set = dbCursor.fetchall()
+                        if(len(set) < 1):
+                            conn.send('Question not found')
+                        else:
+                            dbCursor.execute("DELETE FROM question where id = '%d' "  %(int(userQuestionNum)))
+                            dbObj.commit()
+                            conn.send('Row Deleted')
+                    except Error as error:
+                        print(error)
+                        continue
+                elif userInput == 'g':
+                    # done
+                    userQuestion = conn.recv(1024)
+                    print(userQuestion)
+                    if(userQuestion == '-1'): continue
+                    try:
+                        dbCursor.execute("SELECT * FROM question where id = '%d'" %(int(userQuestion)))
+                        set = dbCursor.fetchall()
+                        if(len(set) < 1):
+                            conn.send('No Question Found')
+                            continue
+                        result = ''
+                        for row in set:
+                            result = str(row[0]) + '\n'
+                            result = result + row[2] + '\n'
+                            result = result + row[3] + '\n'
+                        conn.send(result)
+                    except Error as error:
+                        conn.send('Question not found')
+                        continue
+                elif userInput == 'r':
+                    # done
+                    answer = ''
+                    try:
+                        dbCursor.execute("SELECT * FROM question ORDER BY RAND() LIMIT 1")
+                        set = dbCursor.fetchall()
+                        result = ''
+                        for row in set:
+                            result = str(row[0]) + '\n'
+                            result = result + row[2] + '\n'
+                            result = result + row[3] + '\n'
+                        conn.send(result)
+                    except Error as error:
+                        conn.send('Empty Table')
+                        continue
+                    answer = conn.recv(1024)
+                    print answer
+                    if(row[4] == answer): conn.send('Correct Answer')
+                    else: conn.send('Incorrect Answer')
+                elif userInput == 'c':
+                    # done
+                    userInput = conn.recv(1024)
+                    userInput = userInput.split()
+                    userQuestion = userInput[0]
+                    userAnswer = userInput[1]
+                    print('Userquestion: ' + userQuestion + 'UserAnswer: ' + userAnswer)
+                    try:
+                        sql = "SELECT * FROM question WHERE id = %s"
+                        dbCursor.execute(sql,(userQuestion,))
+                        set = dbCursor.fetchall()
+                        if(len(set) < 1): conn.send('Invalid Arguments')
+                    except Error as error:
+                        conn.send('ERROR with arguements')
+                        continue
+                    for row in set:
+                        if (row[4] == userAnswer): conn.send('Correct Answer')
+                        else: conn.send('Incorrect Answer')
+                elif userInput == 'h':
+                    print('Helpful message already sent.........')
                 else:
-                    dbCursor.execute("DELETE FROM question where id = '%d' "  %(int(userQuestionNum)))
-                    dbObj.commit()
-                    conn.sendall('Row Deleted')
-            except Error as error:
-                print(error)
-                continue
-        elif userInput == 'g':
-            # done
-            userQuestion = conn.recv(1024)
-            print(userQuestion)
-            if(userQuestion == '-1'): continue
-            try:
-                dbCursor.execute("SELECT * FROM question where id = '%d'" %(int(userQuestion)))
-                set = dbCursor.fetchall()
-                if(len(set) < 1):
-                    conn.send('No Question Found')
-                    continue
-                result = ''
-                for row in set:
-                    result = str(row[0]) + '\n'
-                    result = result + row[2] + '\n'
-                    result = result + row[3] + '\n'
-                conn.sendall(result)
-            except Error as error:
-                conn.sendall('Question not found')
-                continue
-        elif userInput == 'r':
-            # done
-            answer = ''
-            try:
-                dbCursor.execute("SELECT * FROM question ORDER BY RAND() LIMIT 1")
-                set = dbCursor.fetchall()
-                result = ''
-                for row in set:
-                    result = str(row[0]) + '\n'
-                    result = result + row[2] + '\n'
-                    result = result + row[3] + '\n'
-                conn.sendall(result)
-            except Error as error:
-                conn.sendall('Empty Table')
-                continue
-            answer = conn.recv(1024)
-            print answer
-            if(row[4] == answer): conn.sendall('Correct Answer')
-            else: conn.sendall('Incorrect Answer')
-        elif userInput == 'c':
-            # done
-            userInput = conn.recv(1024)
-            userInput = userInput.split()
-            userQuestion = userInput[0]
-            userAnswer = userInput[1]
-            print('Userquestion: ' + userQuestion + 'UserAnswer: ' + userAnswer)
-            try:
-                sql = "SELECT * FROM question WHERE id = %s"
-                dbCursor.execute(sql,(userQuestion,))
-                set = dbCursor.fetchall()
-                if(len(set) < 1): conn.sendall('Invalid Arguments')
-            except Error as error:
-                conn.sendall('ERROR with arguements')
-                continue
-            for row in set:
-                if (row[4] == userAnswer): conn.sendall('Correct Answer')
-                else: conn.sendall('Incorrect Answer')
-        elif userInput == 'h':
-            print('Helpful message already sent')
-            # done
-        else:
-            print('sending data back to the client')
-            conn.sendall(userInput)
-    if userInput == 'k':break
+                    print('Sending data back to the client.......')
+                    conn.send(userInput)
+
+def main():
+    socketObj = create_socket()     # Creates socke and returns obj
+    dbObj =  db_setup()             # Create db connection and returns obj
+    client_input(socketObj, dbObj)  # Passes objs to client input function (db and connection access)
+
+main()
